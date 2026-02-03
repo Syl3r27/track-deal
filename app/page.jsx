38dd0c06c3e40f754b489d05de988d9fb43a6e38
Om Prakash Pati <oppati04@gmail.com>
2026-02-03 11:20:44 +0530

    fixes

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ export default async function Home() {
       icon: Rabbit,
       title: "Lightning Fast",
       description:
-        "Deal Drop extracts prices in seconds, handling JavaScript and dynamic content",
+        "TrackDeal Drop extracts prices in seconds, handling JavaScript and dynamic content",
     },
     {
       icon: Shield,
